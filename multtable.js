b9810303d0bdb43ab607d0fe361e0b4f947efeeb
Sebi <167681990+sejager@users.noreply.github.com>
2024-10-27 13:14:55 -0400

    Created the basic dynamic multiplication generator Each time submit is hit the old table is deleted and replaced by an updated one. Still need to add error handling for too large numbers or maximums smaller than the minimums and if there is time somewhat enhance the aesthetics.

--- multtable.js
+++ multtable.js
@@ -4,3 +4,56 @@ Created by Sebastian Gyger
 Part of HW3 for GUI I at UMass Lowell
 This is a JavaScript file to make the dynamic multiplication table dynamic.
 */
+
+function tableCreate() {
+    // Get the values from the submission form to make a table
+    var mincol = document.getElementById("mincol").value;
+    var maxcol = document.getElementById("maxcol").value;
+    var minrow = document.getElementById("minrow").value;
+    var maxrow = document.getElementById("maxrow").value;
+
+    // Get the old table and replace it
+    var tblold = document.getElementById("tbl");
+    tblold.parentElement.removeChild(tblold);
+    const tbl = document.createElement("table");
+    tbl.setAttribute("id", "tbl");
+    const tblBody = document.createElement("tbody");
+
+    var row, cell, cellText;
+    
+    // Create the table
+    for (let i = minrow - 1; i <= maxrow; i++) {
+        row = document.createElement("tr");
+        for (let j = mincol - 1; j <= maxcol; j++) {
+        cell = document.createElement("td");
+        if (i == minrow - 1) {
+            if (j == mincol - 1) {
+                cellText = document.createTextNode("");
+            } else {
+                cellText = document.createTextNode(j)
+            }
+        } else if (j == mincol - 1) {
+            cellText = document.createTextNode(i)
+        } else {
+            cellText = document.createTextNode(i * j);
+        }
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+        }
+    
+        // add the row to the end of the table body
+        tblBody.appendChild(row);
+    }
+    
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    document.body.appendChild(tbl);
+    }
+
+
+var btn = document.getElementById("subbtn");
+btn.addEventListener("click", () => {
+    tableCreate();
+})
+
